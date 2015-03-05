61fb4b44fe4d2760a84426f50f19c77cdf60ed3e
Jeffrey Chan <jeffreychan637@berkeley.edu>
2015-03-05 03:17:36 -0800

    server is now serving client style files

--- server.go
+++ server.go
@@ -11,7 +11,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("%s", r.URL.Path)
+	fmt.Printf("%s\n", r.URL.Path)
 	if r.URL.Path != "/" {
 		http.ServeFile(w, r, "client/views/error.html")
 	} else {
@@ -20,8 +20,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func client(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
 func main() {
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/client/", client)
 	http.HandleFunc("/", home)
 	http.ListenAndServe(":8000", nil)
 }
